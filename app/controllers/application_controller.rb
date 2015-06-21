247ed3a69cf5a988f5874fd3f27c12a988fbcd44
adamyarger <yargeradam@gmail.com>
2015-06-20 19:02:38 -0500

    the pundit auth works with error now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   protect_from_forgery with: :exception
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "Access denied."
+    redirect_to (request.referrer || root_path)
+  end
 end
