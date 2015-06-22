c21c6409ab57b4c7686d07df50fc24b9303b4fbe
adamyarger <yargeradam@gmail.com>
2015-06-22 14:43:24 -0500

    app is ready for casting

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+15.times do |n|
+  email = "example-#{n+1}@example.org"
+  password = "password"
+  User.create!(
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
