e00df75cfd417cf396e400707e8d54bfa51782e1
adamyarger <yargeradam@gmail.com>
2015-06-22 13:57:51 -0500

    use index test pass

---
+++ spec/features/user_index_spec.rb
@@ -0,0 +1,13 @@
+
+
+feature 'User index page', :devise do
+
+
+  scenario 'user sees own email address' do
+    user = FactoryGirl.create(:user, :admin)
+    login_as(user, scope: :user)
+    visit users_path
+    expect(page).to have_content user.email
+  end
+
+end
