901ac6b733eb2b65b4bd774655682a44871414f6
adamyarger <yargeradam@gmail.com>
2015-06-22 13:59:24 -0500

    adds user show page test

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,13 +1,10 @@
-
+require 'rails_helper'
 
 feature 'User index page', :devise do
-
-
   scenario 'user sees own email address' do
     user = FactoryGirl.create(:user, :admin)
     login_as(user, scope: :user)
     visit users_path
     expect(page).to have_content user.email
   end
-
 end
---
+++ spec/features/user_show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'User profile page', :devise do
+
+  scenario 'user sees own profile' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+    visit user_path(user)
+    expect(page).to have_content 'User'
+    expect(page).to have_content user.email
+  end
+
+  scenario "user cannot see another user's profile" do
+    me = FactoryGirl.create(:user)
+    other = FactoryGirl.create(:user, email: 'other@example.com')
+    login_as(me, :scope => :user)
+    Capybara.current_session.driver.header 'Referer', root_path
+    visit user_path(other)
+    expect(page).to have_content 'Access denied.'
+  end
+
+end
