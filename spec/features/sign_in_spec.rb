01fe0b72ad69cb8613387ee3ac515a0b0b4eb197
adamyarger <yargeradam@gmail.com>
2015-06-22 13:50:45 -0500

    passing signin tests

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -11,4 +11,16 @@ feature 'Sign in', :devise do
 		signin(user.email, user.password)
 		expect(page).to have_content 'Signed in successfully.'
 	end
+
+	scenario 'user cannot sign in with wrong email' do
+    user = FactoryGirl.create(:user)
+    signin('invalid@email.com', user.password)
+    expect(page).to have_content 'Invalid email or password.'
+  end
+
+  scenario 'user cannot sign in with wrong password' do
+    user = FactoryGirl.create(:user)
+    signin(user.email, 'invalidpass')
+    expect(page).to have_content 'Invalid email or password.'
+  end
 end
